c01fdf49609fe9421ad167cbf7f62c6fcfcfb2ff
MarsIfeanyi <marschalice@gmail.com>
2023-05-12 23:15:06 +0100

    Add Custom font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
       //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       // },
     },
+
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 };
