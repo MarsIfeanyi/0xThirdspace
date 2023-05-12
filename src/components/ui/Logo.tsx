aa023839b730bec83072dd7147f804d1da4c7f88
MarsIfeanyi <marschalice@gmail.com>
2023-05-12 21:27:33 +0100

    Create Reusable component

---
+++ src/components/ui/Logo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const Logo = (props: Props) => {
+  return (
+    <div>
+      <h1 className="ml-20 text-3xl text-white mt-10">
+        Third<span className="logoGradient">space</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Logo;
