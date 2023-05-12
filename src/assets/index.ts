c383c4fc37c014254f8424fa8dd758c7f2d7f19b
MarsIfeanyi <marschalice@gmail.com>
2023-05-12 23:12:56 +0100

    Add Icons and Images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,6 @@
 import backgroundImage from "./backgorundImage.png";
 import arrowUp from "./arrowUp.svg";
+import signUp from "./signUp.png";
+import signIn from "./signIn.png";
 
-export { backgroundImage, arrowUp };
+export { backgroundImage, arrowUp, signUp, signIn };
