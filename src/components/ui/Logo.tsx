59a0b890a2302e0e6029b5785a7344829baefd5f
MarsIfeanyi <marschalice@gmail.com>
2023-05-16 16:13:25 +0100

    Create Reusable Logo component

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Logo = (props: Props) => {
   return (
     <div>
-      <h1 className="ml-20 text-3xl text-white mt-10 font-roboto font-normal ">
+      <h1 className="ml-20 text-3xl text-white mt-10 font-roboto ">
         Third<span className="logoGradient">space</span>
       </h1>
     </div>
