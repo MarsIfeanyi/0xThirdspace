82bca094b855304af8492f1895ece4121002c164
MarsIfeanyi <marschalice@gmail.com>
2023-06-02 17:20:19 +0100

    Create BuntyHunter Page

---
+++ src/pages/hunter/index.tsx
@@ -0,0 +1,17 @@
+import AvailableBounties from "@/components/Creator/AvailableBounties";
+
+import React from "react";
+
+AvailableBounties;
+
+type Props = {};
+
+const index = (props: Props) => {
+  return (
+    <div>
+      <AvailableBounties />
+    </div>
+  );
+};
+
+export default index;
