27ff4ff0e26d54c7a4fa4f5c2837236e4219e7a6
MarsIfeanyi <marschalice@gmail.com>
2023-06-02 17:19:50 +0100

    Crearte and Add Ui folder and Component

---
+++ src/components/Ui/Logo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const Logo = (props: Props) => {
+  return (
+    <div>
+      <h1 className=" text-3xl text-white   font-nexa ">
+        Third<span className="logoGradient">space</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/Ui/Modal.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+type Props = {
+  isVisible: boolean;
+  onClose: boolean | void | string | any;
+  children: React.ReactNode;
+};
+
+const Modal = ({ isVisible, onClose, children }: Props) => {
+  if (!isVisible) return null;
+
+  const handleClose = (e: any) => {
+    if (e.target.id === "wrapper") onClose();
+  };
+
+  return (
+    <div
+      className="fixed inset-0 bg-[#05050533] bg-opacity-25 backdrop-blur-lg  flex justify-center items-center"
+      id="wrapper"
+      onClick={handleClose}
+    >
+      <div className="w-[700px] flex flex-col relative ">
+        <button
+          className="text-white text-2xl absolute top-6 right-10   "
+          onClick={() => onClose()}
+        >
+          X
+        </button>
+        <div className="bg-[#0F0F0F] p-2 rounded">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
