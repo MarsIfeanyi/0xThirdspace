dda978d3e73eae134025591a65ea8bef45e518f8
MarsIfeanyi <marschalice@gmail.com>
2023-06-03 01:40:02 +0100

    Render AvailableBounties component on the page

---
+++ src/pages/available/index.tsx
@@ -0,0 +1,14 @@
+import AvailableBounties from "@/components/AvailableBounties/AvailableBounties";
+import React from "react";
+
+type Props = {};
+
+const AvailableBountyPage = (props: Props) => {
+  return (
+    <div>
+      <AvailableBounties />
+    </div>
+  );
+};
+
+export default AvailableBountyPage;
