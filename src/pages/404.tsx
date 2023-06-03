86bc45715fa207f1ea6aef94e2b5ca163111481c
MarsIfeanyi <marschalice@gmail.com>
2023-06-03 01:41:25 +0100

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const NotFoundPage = (props: Props) => {
+  return (
+    <div className="text-center justify-center items-center mx-auto font-nexa font-semibold text-2xl  text-white mt-32 ">
+      <h2> Hey friend, sorry requested page not found</h2>
+    </div>
+  );
+};
+
+export default NotFoundPage;
