7e94a9ac473ef979e7a65af2bf2371f447811b68
MarsIfeanyi <marschalice@gmail.com>
2023-06-03 01:42:13 +0100

    Create bountySpaceName page

---
+++ src/pages/bounty-space.tsx
@@ -0,0 +1,14 @@
+import CreateBountySpace from "@/components/Home/CreateBountySpace";
+import React from "react";
+
+type Props = {};
+
+const CreateBountySpacePage = (props: Props) => {
+  return (
+    <div>
+      <CreateBountySpace />
+    </div>
+  );
+};
+
+export default CreateBountySpacePage;
