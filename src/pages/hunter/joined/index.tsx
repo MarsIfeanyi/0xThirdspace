d83f43c36ffc3e6b0063f7ede5c6380af6a497f5
MarsIfeanyi <marschalice@gmail.com>
2023-06-04 23:11:06 +0100

    Create JoinedBounty Page

---
+++ src/pages/hunter/joined/index.tsx
@@ -0,0 +1,16 @@
+import JoinedBounty from "@/components/Hunter/Bounties/JoinedBounties";
+import React from "react";
+
+JoinedBounty;
+
+type Props = {};
+
+const index = (props: Props) => {
+  return (
+    <div>
+      <JoinedBounty />
+    </div>
+  );
+};
+
+export default index;
