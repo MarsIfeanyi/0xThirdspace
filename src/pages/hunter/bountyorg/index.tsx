6b36b00cc342dbd30d3d0a7749f0f8c0b752aadf
MarsIfeanyi <marschalice@gmail.com>
2023-06-04 23:10:29 +0100

    Create BountyOrganisationPage

---
+++ src/pages/hunter/bountyorg/index.tsx
@@ -0,0 +1,14 @@
+import BountyOrganization from "@/components/Hunter/Bounties/BountyOrganization";
+import React from "react";
+
+type Props = {};
+
+const BountyOrganizationPage = (props: Props) => {
+  return (
+    <div>
+      <BountyOrganization />
+    </div>
+  );
+};
+
+export default BountyOrganizationPage;
