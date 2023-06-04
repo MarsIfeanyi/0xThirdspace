1cece18e5c575f51646fcc2bfc079c6893569550
MarsIfeanyi <marschalice@gmail.com>
2023-06-04 23:11:50 +0100

    Create Members Page

---
+++ src/pages/hunter/members/index.tsx
@@ -0,0 +1,14 @@
+import Members from "@/components/Hunter/Members/Members";
+import React from "react";
+
+type Props = {};
+
+const index = (props: Props) => {
+  return (
+    <div>
+      <Members />
+    </div>
+  );
+};
+
+export default index;
